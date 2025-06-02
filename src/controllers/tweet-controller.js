0664ed97b4899f48a1f61451dc99fdf49a0a282e
Ekam Singh <ekamsinghh24@gmail.com>
2025-06-03 02:08:39 +0530

    Added tweet upload functionality and fixed create tweets

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,9 +1,20 @@
 import TweetService from '../services/tweet-service.js';
+import upload from '../config/file-upload-s3-config.js';
 
 const tweetService = new TweetService();
+const singleUploader= upload.single('image');//* this 'image' is the key that we will get from frontend inorder to upload image
 
 export const createTweet = async (req, res) => {
     try {
+        // singleUploader(req,res,function(err,data){
+        //     if(err) {
+        //         return res.status(500).json({error: err});
+        //     }
+        //     console.log('Image url is: ',req.file);
+        // return res.status(200).json({
+        //     message: 'Image uploaded successfully'
+        // })
+        // })
         const tweet = await tweetService.create(req.body);
         return res.status(201).json({
             message: 'Tweet created successfully',
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -11,26 +11,31 @@ class TweetService {
         try{
             const content=data.content;
             let tags= content.match(/#[a-zA-Z0-9_]+/g)//* this regex extracts hashtags
-            tags=tags.map((element)=>{
-                return element.substring(1).toLowerCase();
-            });
-            const tweet= await this.tweetrepo.create(data);
-            const tweetId=tweet.id;
-            let existingTitles= await this.hashtagrepo.findByName(tags);
-            let titleList=existingTitles.map((tag)=>{
-                return tag.title;
-            });//* array of titles already present in database
-            // existingTitles=tags.filter((tag) => titleList.includes(tag));
-            // console.log(existingTitles);
-            let newTags=tags.filter((tag) => !titleList.includes(tag));
-            newTags = newTags.map((tag)=>{
-                return {title: tag, tweets: [tweetId]};
-            })
-            await this.hashtagrepo.bulkCreate(newTags);
-            existingTitles.forEach((tag) => {
-                tag.tweets.push(tweetId);
-                tag.save();
-            })
+            if(tags){
+                tags=tags.map((element)=>{
+                    return element.substring(1).toLowerCase();
+                });
+                var tweet= await this.tweetrepo.create(data);
+                const tweetId=tweet.id;
+                let existingTitles= await this.hashtagrepo.findByName(tags);
+                let titleList=existingTitles.map((tag)=>{
+                    return tag.title;
+                });//* array of titles already present in database
+                // existingTitles=tags.filter((tag) => titleList.includes(tag));
+                // console.log(existingTitles);
+                let newTags=tags.filter((tag) => !titleList.includes(tag));
+                newTags = newTags.map((tag)=>{
+                    return {title: tag, tweets: [tweetId]};
+                })
+                await this.hashtagrepo.bulkCreate(newTags);
+                existingTitles.forEach((tag) => {
+                    tag.tweets.push(tweetId);
+                    tag.save();
+                });
+            }
+            else{
+                var tweet= await this.tweetrepo.create(data);
+            }
             return tweet;
         }
         catch(error){
